b34d766fbbbcf16b3dd658e4ae0bac5fa7d4d99a
Rune-Meillhede <64023@edu.campusvejle.dk>
2026-05-12 11:51:35 +0200

    hjh

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <!-- custom color css style sheet -->
     <link rel="stylesheet" href="colors.css">
-    <title>Vejle fritids aktiviteter</title>
+    <title>Vejle fritids aktiviteter test</title>
 </head>
 
 <body>
